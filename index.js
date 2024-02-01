bbf573e492f8590aa0692ece133d59926664a0bb
Mahmoud Abo-Hgr <ma5027300@gmail.com>
2024-02-01 19:05:18 +0200

    handling port issue

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 5000 || port;
+const port = 5000 || process.env.PORT;
 const dataBaseeConnection = require('./db/database.config');
 const bodyParser = require('body-parser');
 
