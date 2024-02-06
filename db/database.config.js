113abbb556fcf32f6497a947a4a1aef25d26cf21
Mahmoud Abo-Hgr <ma5027300@gmail.com>
2024-02-06 23:04:16 +0200

    adding delete list and delete task feature

--- db/database.config.js
+++ db/database.config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dataBaseeConnection= ()=>{
     console.log();
-    mongoose.connect(process.env.DATABASELOCALURL)
+    mongoose.connect(process.env.DATABASE_REMOTE_URL)
     .then(()=> console.log("database connected successfully"))
     .catch((err)=> console.log("database conncetion failed\n"+err))
 }
